06e57b38ece447352894ec504177d804d77ebe02
Bruno Gianini <blgianini@gmail.com>
2025-05-19 20:50:49 -0300

    feat: poc inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import GridItem from "@/components/grid-item";
-import ItemTable from "@/components/item-talbe";
 
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen gap-10 flex-col">
-      <ItemTable />
-      <GridItem />
+      
     </main>
   );
 }
